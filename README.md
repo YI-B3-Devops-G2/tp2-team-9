9077ef353a3d97db01ee6a9d590aaa76f0dc127c
HugoYnovLyon <hugo.morillon@ynov.com>
2020-03-08 20:35:15 +0100

    second commit
    ratrappage avec l'existant

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# tp2-team-9
+# B3 Devops TP2 - Team 9
+
+## Informations
+
+### Team 9 - Composition
+
+- Mail: [hugo.morillon@ynov.com](mailto:hugo.morillon@ynov.com)
+  Github_username: HugoYnovLyon
+- Mail: [melanie.tripot@ynov.com](mailto:melanie.tripot@ynov.com)
+  Github_username: MTripot
---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const { Client } = require('pg');
+const redis = require('redis');
+
+const pgClient = new Client({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: 5432,
+});
+const redisClient = redis.createClient({ host: 'devops_tp_redis' });
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api', (req, res) => {
+    res.json({ message: 'Hello World' });
+});
+
+const run = async () => {
+    try {
+        await pgClient.connect();
+        const port = process.env.PORT;
+        app.listen(port, () => console.log(`Server started. PORT : ${port}`));
+    } catch (e) {
+        console.log(e);
+        console.error('Unable to connect to the postgreSQL database');
+    }
+};
+
+run();
